bd104e3d15004818bfa83c62a7c7a3f40c8aef4b
staka99 <marko.staka12@gmail.com>
2024-09-27 17:32:09 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    &nbsp;
+    <router-link to="/finished-tasks">Finished Tasks</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  // Methods
+  methods: {
+    addTask(task) {
+      this.tasks.push(task);
+    },
+    deleteTask(taskId) {
+      this.tasks = this.tasks.filter(task => task.id !== taskId);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
